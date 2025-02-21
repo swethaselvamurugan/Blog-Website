675f205b453768ef4d02aa0d65bcb10722499e6d
swethaselvamurugan <swetha040725@gmail.com>
2025-02-21 11:24:47 +0530

    -Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
           <Route path="/home" element={<Home />}></Route>
           <Route path="/blogs" element={<Blogs />}></Route>
           <Route path="/login" element={<Login />}></Route>
